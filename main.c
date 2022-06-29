5c3fe80b09d612e76b86c74f255721fbf93d7287
Partokus <lepeshkin-av-97@mail.ru>
2022-06-29 13:04:27 +0300

    step 2

--- main.c
+++ main.c
@@ -17,14 +17,25 @@ int main(void)
 	
     while (1) 
     {
-		if (time_board_led < time_ms)
+		if (time_counter_board_led_blink < time_ms)
 		{
-			time_board_led = time_ms + 1000;
+			time_counter_board_led_blink = time_ms + 1000;
 			
-			board_led_status = !board_led_status;
-			if (board_led_status) PORTD |= 1 << pin_board_led;
-			else PORTD &= ~(1 << pin_board_led);
+			//board_led_status = !board_led_status;
+			//if (board_led_status) PORTD |= 1 << pin_board_led;
+			//else PORTD &= ~(1 << pin_board_led);
 		}
+		
+		if (time_counter_buttons < time_ms)
+		{
+			time_counter_buttons = time_ms + 1;
+			
+			button_tick(&button_on_off);
+			button_tick(&button_reset);
+			button_tick(&button_red);
+		}
+		
+		
     }
 }
 
@@ -34,6 +45,15 @@ void init_port()
 	DDRD = 1 << pin_board_led;
 	PORTD = 1 << pin_board_led;
 	board_led_status = true;
+	
+	// buttons
+	// on/off button
+	DDRB = 0;
+	PORTB = 1 << pin_button_on_off;
+	// reset button
+	PORTB = 1 << pin_button_reset;
+	// red button
+	PORTD |= 1 << pin_button_red;
 }
 
 void init_timers()
@@ -48,4 +68,86 @@ void init_timers()
 ISR(TIMER0_COMPA_vect)
 {
 	time_ms++;
+}
+
+// process button
+void button_tick(struct buttons *button)
+{
+	bool pin_level = ((1 << button->pin) & *button->port);
+	uint32_t past_time;
+	
+	if (button->stage != passive)
+	{
+		past_time = time_ms - button->time_counter; 
+	}
+	
+	switch (button->stage)
+	{
+		case passive:
+			if (pin_level == false) 
+			{
+				button->stage = debounce_press;
+				button->time_counter = time_ms;
+			}
+		break;
+		
+		case debounce_press:
+			if (past_time >= buttons_debounce_time) 
+			{
+				button->stage = interference;
+			}
+		break;
+		
+		case interference:
+			if (pin_level == true) // if we catch interference
+			{
+				button->stage = passive; // stop proccess button
+			}
+			
+			if (past_time >= (buttons_interference_time + buttons_debounce_time))
+			{
+				button->stage = waiting_release;
+				
+				if (button->type == single_click_press) button->button_callback();
+			}
+		break;
+		
+		case waiting_release:
+			if (pin_level == true)
+			{
+				if (button->type == long_click) button->button_callback(); // if button was released before long click called
+				
+				button->stage = debounce_release;
+				button->time_counter = time_ms;
+			} else if (button->type == long_click) {
+				if (past_time >= button->time_for_long_click) // if time for long click became
+				{
+					button->stage = waiting_release_longclick;
+					button->button_callback();
+				}
+			}
+		break;
+		
+		case waiting_release_longclick:
+			if (pin_level == true)
+			{	
+				button->stage = debounce_release;
+				button->time_counter = time_ms;
+			}
+		break;
+		
+		case debounce_release:
+			if (past_time >= buttons_debounce_time)
+			{
+				button->stage = passive;
+			}
+		break;
+	}
+}
+
+void button_on_off_callback()
+{
+	board_led_status = !board_led_status;
+	if (board_led_status) PORTD |= 1 << pin_board_led;
+	else PORTD &= ~(1 << pin_board_led);
 }
--- sacumi_melanjor_ATtiny2313.h
+++ sacumi_melanjor_ATtiny2313.h
@@ -11,18 +11,87 @@
 
 #include <stdbool.h>
 
-// functions
-void init_port();
-void init_timers();
+void button_on_off_callback();
+
+// config
+#define buttons_debounce_time 10
+#define buttons_interference_time 100
 
 // pins
 #define pin_board_led PD0
+//buttons
+#define pin_button_on_off PB1
+#define pin_button_reset PB0
+#define pin_button_red PD6
 
 // variables
 bool board_led_status = false;
 uint32_t time_ms = 0;
-uint32_t time_board_led = 0;
+uint32_t time_counter_board_led_blink = 0;
+uint32_t time_counter_buttons = 0;
 
+
+// enums
+enum button_types {
+	single_click_press = 0,
+	single_click_release = 1,
+	long_click = 2
+};
+enum button_stages {
+	passive = 0,
+	debounce_press = 1,
+	interference = 2,
+	waiting_release = 3,
+	waiting_release_longclick = 4,
+	debounce_release = 5
+};
+
+// structures
+struct buttons 
+{
+	uint8_t pin;
+	volatile uint8_t *port;
+	uint8_t type;
+	uint32_t time_counter;
+	uint8_t stage;
+	uint32_t time_for_long_click; // ms. how long press button until register long click
+	void(*button_callback)(void);
+};
+
+struct buttons button_on_off = {
+	pin_button_on_off,
+	&PINB,
+	single_click_press,
+	0,
+	passive,
+	0,
+	button_on_off_callback
+};
+
+struct buttons button_reset = {
+	pin_button_reset,
+	&PINB,
+	long_click,
+	0,
+	passive,
+	3000,
+	button_on_off_callback
+};
+
+struct buttons button_red = {
+	pin_button_red,
+	&PIND,
+	long_click,
+	0,
+	passive,
+	3000,
+	button_on_off_callback
+};
+
+// functions
+void init_port();
+void init_timers();
+void button_tick(struct buttons *button);
 
 
 
